7269c96b0c7f0c52a88083da53c9adc6ca59ec90
linkan chauhan <linkanchauhan610@gmail.com>
2020-02-16 05:54:13 +0530

    Product Filter algorithm based on ranking
    Product Filter algorithm based on ranking

--- app/src/main/java/linkan/a740362/testecommerceapp/util/UtilFunction.kt
+++ app/src/main/java/linkan/a740362/testecommerceapp/util/UtilFunction.kt
@@ -15,150 +15,21 @@ object UtilFunction {
         val rankings: JSONArray = if (json.has("rankings")) json.getJSONArray("rankings") else JSONArray()
 
 
-        /*    recursiveSubcategory(
-               category,
-               category.getJSONObject(0).getJSONArray("child_categories"),
-               0,
-               0,
-               0,
-               0
-           )
-
-            outer@ while (parentCount < category.length()) {
-
-                 if (category.getJSONObject(parentCount).has("child_categories")
-                     && category.getJSONObject(parentCount).getJSONArray("child_categories").length() > 0
-                 ) {
-                     var childCount = 0
-
-                     inner@ while (childCount < category.getJSONObject(parentCount).getJSONArray("child_categories").length()) {
-
-                     }
-
-                 } else
-                     parentCount++
-
-             }*/
-
         return filterProductByCategory(category, rankings).toString()
-    }
-
-
-    fun recursiveSubcategory(
-        category: JSONArray,
-        child_category: JSONArray,
-        parentCount: Int,
-        childCount1: Int,
-        childCount2: Int,
-        childId: Int
-    ) {
-
-        if (child_category.length() > 0 || childCount2 < category.length()) {
-
-
-            if (childCount1 < child_category.length() && child_category.length() > 0) {
-
-                if (childCount2 < category.length() && category.length() > 0) {
-
-                    if (childId == category.getJSONObject(childCount2).getInt("id")) {
-
-                        val subCategory1 = category.getJSONObject(childCount2)
-
-                        // remove it from original category
-                        category.remove(childCount2)
-                        // add to the parent category as a child subcategory
-                        child_category.put(childCount1, subCategory1)
-
-                        if (subCategory1.getJSONArray("child_categories").length() > 0) {
-
-                        }
-
-                    } else
-                        recursiveSubcategory(
-                            category,
-                            child_category,
-                            parentCount,
-                            childCount1,
-                            childCount2 + 1,
-                            child_category.getInt(childCount1)
-                        )
-
-
-                } else
-                    recursiveSubcategory(
-                        category,
-                        child_category,
-                        parentCount,
-                        childCount1 + 1,
-                        0,
-                        child_category.getInt(childCount1 + 1)
-                    )
-
-            } else
-                recursiveSubcategory(
-                    category,
-                    category.getJSONObject(parentCount + 1).getJSONArray("child_categories"),
-                    parentCount + 1,
-                    childCount1,
-                    childCount2,
-                    childId
-                )
-
-
-
-            outer@ while (childCount1 < child_category.length()) {
-
-                if (child_category.get(childCount1) is Int) {
-
-                    val childId = child_category.getInt(childCount1)
-
-                    inner@ while (childCount2 < category.length()) {
-
-                        if (childId == category.getJSONObject(childCount2).getInt("id")) {
-
-                            val subCategory = category.remove(childCount2)
-
-                            child_category.put(childCount1, subCategory)
-
-                        }
-                    }
-
-                    recursiveSubcategory(
-                        category,
-                        child_category,
-                        parentCount,
-                        0,
-                        0,
-                        child_category.getInt(childCount1)
-                    )
-                }
-
-            }
-
-        } else {
-
-            recursiveSubcategory(
-                category,
-                category.getJSONObject(parentCount + 1).getJSONArray("child_categories"),
-                parentCount + 1,
-                0,
-                0,
-                0
-            )
-
-        }
-
     }
 
 
     //-----------------------------------------------------
 
-    fun filterProductByCategory(category: JSONArray, rankings: JSONArray): JSONArray {
+    fun filterProductByCategory(category: JSONArray, rankings: JSONArray): JSONObject {
 
         val response = JSONArray()
+        val responseObj = JSONObject()
 
         val categoryMap = SparseArray<JSONObject>()
         val subCategoryMap = SparseIntArray()
+
+        val flatProductMap = SparseArray<JSONObject>()
 
 
         for (i in 0 until category.length()) {
@@ -171,6 +42,18 @@ object UtilFunction {
                 val subCategoryId = catObject.getJSONArray("child_categories").getInt(j)
 
                 subCategoryMap.put(subCategoryId, catObject.getInt("id"))
+            }
+
+            // creating  products flat map
+            for (k in 0 until catObject.getJSONArray("products").length()) {
+
+                val productObj = catObject.getJSONArray("products").getJSONObject(k)
+
+                flatProductMap.put(
+                    productObj.getInt("id"),
+                    productObj
+                )
+
             }
         }
 
@@ -199,17 +82,6 @@ object UtilFunction {
 
                 val categoryId = subCategoryMap.get(catObject.getInt("id")) // value for subcategory
 
-                /*    val subCategoryObj = category.getJSONObject(subCategoryId) // accessed the subcategory based on position
-
-                    category.remove(subCategoryId) // remove category json object
-
-                    category.put(
-                        categoryId,
-                        category
-                            .getJSONObject(categoryId)
-                            .getJSONArray("child_categories")
-                            .put(categoryId, subCategoryObj)
-                    )*/
 
                 val subCategoryObj = categoryMap.get(subCategoryId) // accessed the subcategory based on position
 
@@ -218,13 +90,7 @@ object UtilFunction {
 
                 val jsonObject = categoryMap.get(categoryId)  // extract json object
 
-                /*  for (k in 0 until jsonObject.getJSONArray("child_categories").length()) {
-                      if (jsonObject.getJSONArray("child_categories").getInt(k) == subCategoryId) {
 
-                          jsonObject.getJSONArray("child_categories").remove()
-
-                      }
-                  }*/
 
                 jsonObject.getJSONArray("child_categories")  // set json aaray
                     .put(subCategoryObj)
@@ -236,13 +102,53 @@ object UtilFunction {
 
         }
 
+        // code to short product based on rank
+        for (i in 0 until rankings.length()) {
+
+            val productArray = rankings.getJSONObject(i).getJSONArray("products")
+
+            for (j in 0 until productArray.length()) {
+
+                var key = "view_count"
+                var valueCount = 0L
+
+                val productObj = productArray.getJSONObject(j)
+
+                val mapProductObj = flatProductMap.get(productObj.getInt("id"))
+
+
+                if (productObj.has("view_count")) {
+                    key = "view_count"
+                } else if (productObj.has("order_count")) {
+                    key = "order_count"
+                } else if (productObj.has("shares")) {
+                    key = "shares"
+                }
+
+                valueCount = productObj.getLong(key)
+
+                // changing map product object
+                mapProductObj.put("view_count", valueCount)
+
+                // remove old product object from product array
+                //  productArray.remove(j)
+
+                // adding updated product array in product array
+                productArray.put(j, mapProductObj)
+            }
+
+        }
+
         for (i in 0 until category.length()) {
             val obj = categoryMap.get(i, JSONObject())
             if (obj.length() > 0)
                 response.put(categoryMap.get(i, obj))
         }
 
-        return response
+        responseObj.put("categories", response)
+        responseObj.put("rankings", rankings)
+
+        return responseObj
 
     }
 
